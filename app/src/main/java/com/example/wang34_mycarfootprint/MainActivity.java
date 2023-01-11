c1386b5ab17f1b1090bb8cb5e9a778512ab5e71e
Yang Wang <1527638985@qq.com>
2023-01-10 20:39:53 -0700

    Reconstructed update

--- app/src/main/java/com/example/wang34_mycarfootprint/MainActivity.java
+++ app/src/main/java/com/example/wang34_mycarfootprint/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.wang34_mycarfootprint;
 
-import androidx.activity.result.ActivityResult;
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
